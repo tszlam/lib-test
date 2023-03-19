2afa3c6a15170b786e3bd8387374ac240fa170f1
tszlamli <tszlamli@tencent.com>
2023-03-19 21:40:09 +0800

    调整下config文件已获取类型提醒

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,12 @@
-module.exports = {
+import { defineConfig } from 'rollup'
+
+const config = defineConfig({
   input: './src/index.js',
   output: ['amd', 'cjs', 'es', 'iife', 'umd', 'system'].map(format => ({
     file: `./dist/bundle-${format}.js`,
     format,
     name: 'utils', // UNM 需要这个，但是不知道这个是干嘛的
   }))
-}
+})
+
+export default config
