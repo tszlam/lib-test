7af5c01df73b0a68cba2ebab0f40cff5e7f0ae96
tszlamli <tszlamli@tencent.com>
2023-03-20 17:52:03 +0800

    fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,13 +3,22 @@ import typescript from '@rollup/plugin-typescript'
 
 const config = defineConfig({
   input: 'src/index.ts',
-  output: ['amd', 'cjs', 'es', 'iife', 'umd', 'system'].map(format => ({
-    file: `./dist/index-${format}.js`,
-    format,
-    name: 'utils', // UNM 需要这个，但是不知道这个是干嘛的
-  })),
+  // output: ['amd', 'cjs', 'es', 'iife', 'umd', 'system'].map(format => ({
+  //   file: `./dist/index-${format}.js`,
+  //   format,
+  //   name: 'utils', // UNM 需要这个，但是不知道这个是干嘛的
+  // })),
+
+  output: {
+    file: "./dist/index.js",
+    format: "es",
+  },
   plugins: [
-    typescript(),
+    typescript({
+      module: 'esnext',
+      declaration: true,
+      outDir: './dist',
+    }),
   ]
 })
 
